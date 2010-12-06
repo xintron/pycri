432606285ca906e6b870634eab03e5557587523f
Marcus Fredriksson <drmegahertz@gmail.com>
2010-12-06 17:37:23 +0100

    Adds the help plugin

--- plugins/dice.py
+++ plugins/dice.py
@@ -6,18 +6,7 @@ from plugins import Plugin, command
 class Dice(Plugin):
     @command
     def roll(self, dice):
-        '''
-        Rolls a dice according to roleplaying notation::
-
-            2d6 = Sum of two six-sided dices
-            1d10 = A single ten-sided dice
-
-        Example::
-
-            !roll 2d6
-            !roll 2d100
-        '''
-
+        '''Rolls a dice according to roleplaying notation. Example: !roll 2d6, rolls two 6-sided dices.'''
         multiplier, sides = (int(x) for x in dice.split('d'))
         result = random.choice(xrange(multiplier, multiplier * sides + 1))
 
--- plugins/load.py
+++ plugins/load.py
@@ -4,16 +4,22 @@ from plugins import Plugin, command
 class LoadPlugin(Plugin):
     @command
     def load(self, name):
+        '''Loads the specified plugin. Example: !load dice'''
         Plugin.load(name)
 
     @command
     def unload(self, name):
+        '''Unloads the specified plugin. Example: !unload dice'''
         Plugin.unload(name)
 
     @command
     def reload(self, name='all'):
-        if name == 'all':
-            for module in Plugin.library:
-                Plugin.reload(module)
-        else:
-            Plugin.reload(name)
+        '''Reloads the specified plugin. Example: !reload dice'''
+        try:
+            if name == 'all':
+                for module in Plugin.library:
+                    Plugin.reload(module)
+            else:
+                Plugin.reload(name)
+        except KeyError:
+            return 'No such plugin is currently loaded. Try !load {0}'.format(name)
