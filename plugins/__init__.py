528089c0ca8580258bafd9a615fb00d2237a5f32
Marcus Fredriksson <drmegahertz@gmail.com>
2010-12-07 17:52:19 +0100

    Adds support for multiple plugins per module

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,15 +1,20 @@
+import collections
 import imp
 import inspect
-
-import settings
 
 
 class PluginLibrary(type):
     def __init__(cls, name, bases, attrs):
-        if not hasattr(cls, 'library'):
-            cls.library = {}
+        module_name = cls.__module__.lower()
+        class_name = name.lower()
+
+        if not hasattr(cls, 'cache'):
+            cls.cache = collections.defaultdict(dict)
+
         else:
-            cls.library[cls.__module__] = cls()
+            # Do not load classes that's already cached
+            if not class_name in cls.cache[module_name]:
+                cls.cache[module_name][class_name] = cls
 
         type.__init__(cls, name, bases, attrs)
 
@@ -18,46 +23,85 @@ class Plugin(object):
     __metaclass__ = PluginLibrary
 
     commands = {}
+    library = collections.defaultdict(dict)
 
     @classmethod
     def load(cls, name):
-        fp, pathname, description = imp.find_module(name, ['plugins'])
+        module_name, plugin_name = name.split('.') if '.' in name else (name, '')
 
+        # Load module to register new Plugin subclasses
         try:
-            imp.load_module(name, fp, pathname, description)
-
-            # Collect the methods that's marked as commands
-            for name, obj in inspect.getmembers(cls.library[name]):
-                if not name.startswith('_') and inspect.ismethod(obj):
-                    if hasattr(obj, 'command'):
-                        commands = [obj.command] + obj.aliases
-
-                        for command in commands:
-                            cls.commands[command] = obj
+            fp, pathname, description = imp.find_module(module_name, ['plugins'])
+            imp.load_module(module_name, fp, pathname, description)
 
         finally:
             if fp:
                 fp.close()
 
+        plugins_to_instantiate = []
+
+        if plugin_name:
+            # Load specific plugin from module
+            plugin = cls.cache[module_name][plugin_name]
+            plugins_to_instantiate.append((plugin_name, plugin))
+
+        else:
+            # Load all plugins in the module
+            for name, plugin in cls.cache[module_name].iteritems():
+                plugins_to_instantiate.append((name, plugin))
+
+        # Instantiate plugins and collect commands
+        for name, plugin in plugins_to_instantiate:
+            if not name in cls.library[module_name]:
+                plugin_instance = plugin()
+
+                for method_name, method in inspect.getmembers(plugin_instance):
+                    if not method_name.startswith('_') and inspect.ismethod(method):
+                        if hasattr(method, 'command'):
+                            commands = [method.command] + method.aliases
+
+                            for command in commands:
+                                cls.commands[command] = method
+
+                cls.library[module_name][name] = plugin_instance
+
     @classmethod
     def unload(cls, name):
-        plugin = cls.library[name]
+        module_name, plugin_name = name.split('.') if '.' in name else (name, '')
 
+        # Collect commands to unload
         commands_to_unload = []
         for command, method in cls.commands.iteritems():
-            if method.im_self is plugin:
+            unload = False
+
+            if plugin_name:
+                if method.im_self is cls.library[module_name][plugin_name]:
+                    unload = True
+            else:
+                if method.im_self in cls.library[module_name].values():
+                    unload = True
+
+            if unload:
                 commands_to_unload.append(command)
 
+        # Unload the commands
         for command in commands_to_unload:
             del cls.commands[command]
 
-        del cls.library[name]
+        # Unload single plugin or the entire module
+        if plugin_name:
+            del cls.cache[module_name][plugin_name]
+            del cls.library[module_name][plugin_name]
 
+        else:
+            del cls.cache[module_name]
+            del cls.library[module_name]
 
     @classmethod
     def reload(cls, name):
         cls.unload(name)
         cls.load(name)
+
 
 def command(func=None, name='', aliases=None):
     if aliases is None:
--- plugins/core.py
+++ plugins/core.py
@@ -1,11 +1,29 @@
 from plugins import Plugin, command
 
 
-class LoadPlugin(Plugin):
+class Help(Plugin):
+    @command
+    def help(self, command):
+        '''Displays a brief explanation of a given command. Example: !help help'''
+        try:
+            command_documentation = self.commands[command].__doc__
+        except KeyError:
+            return 'No such command.'
+
+        if not command_documentation:
+            command_documentation = 'No documentation found for !{}.'.format(command)
+
+        return command_documentation
+
+
+class Load(Plugin):
     @command
     def load(self, name):
         '''Loads the specified plugin. Example: !load dice'''
-        Plugin.load(name)
+        try:
+            Plugin.load(name)
+        except ImportError:
+            return 'No such plugin.'
 
     @command
     def unload(self, name):
